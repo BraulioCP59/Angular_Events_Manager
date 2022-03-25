607c72d534dac15a36d39ab1980b60e289277b40
Braulio Mora <brauliocdc59@gmail.com>
2022-03-25 12:47:03 -0700

    updated app comp, added project images and styles

---
+++ events-manager/src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<h2> Hello World </h2>',
+  styles: []
+})
+export class EventsAppComponent {
+  title = 'events-manager';
+}
