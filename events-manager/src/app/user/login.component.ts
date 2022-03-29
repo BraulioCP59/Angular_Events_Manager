c99e97db8d5dfe1020c03af1354379d13d1d0370
Braulio Mora <brauliocdc59@gmail.com>
2022-03-29 11:19:51 -0700

    implemented form field validations for the login form

--- events-manager/src/app/user/login.component.ts
+++ events-manager/src/app/user/login.component.ts
@@ -3,12 +3,16 @@ import { Router } from "@angular/router";
 import { AuthService } from "./auth.service";
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: [`
+        em {float: right; color: #E05C65; padding-left: 10px; }
+    `]
 })
 export class LoginComponent
 {
     userName:string = ''
     password:string = ''
+    mouseoverLoging:boolean = false
 
     constructor(private authService:AuthService, private router:Router)
     {
@@ -25,4 +29,5 @@ export class LoginComponent
     {
         this.router.navigate(['events']);
     }
+
 }
